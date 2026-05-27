8bf45a7d7231cf7945f6759e4af8ec07eaca3d77
Morgen Swartz <morgenswartz619@gmail.com>
2026-05-28 01:02:41 +0200

    Add Exercise 02 - Everything

---
+++ week7_ex02_everything.js
@@ -0,0 +1,13 @@
+// TODO: Create a function called sum. The function will take in a parameter and calculate all the numbers from 0 to the parameter. You must check if the parameter is an integer first before any calculation is made. If the parameter is not a number, return a message stating: "The value passed is not a number". You are NOT allowed to use methods such as !Number.isInteger(n) or regular expressions. Make sure to test your code effectively.
+
+// TODO: Create a function called 'factorial' that takes in a number as a parameter. The function will print the factorial of the entered number. Example: factorial(4) outputs 4*3*2*1 which equals 24.
+
+// TODO: Create a function called funkyMath. If this function is called with 2 arguments, subtract the first from the second. If called with 3 arguments, add all 3 numbers together. If called with 4 arguments, add argument 1 and 2 together and argument 3 and 4 together separately, then divide them. Example: funkyMath(8,2,3,5) produces 8+2 divided by 3+5, which is 10 divided by 8, output 1.25.
+
+// TODO: Create a loop that will remove all the odd numbers from the array and add them to a new array. Use the following array: [1, 2, 33, 45, 6, 44]. Bonus: arrange the odd numbers from smallest to biggest.
+
+// TODO: Create an object called 'me' with properties of first name, last name, age, favourite colour, and dream car.
+
+// TODO: Create and add a new property and value of 'favourite food' to the object.
+
+// TODO: Delete the age property from the object.
